3082f10cfa4bfc6164a2a9e312e6bee2ba843788
pegas <vadim-pegas@ya.ru>
2008-11-24 10:45:01 +0000

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,3 +17,14 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = false
+
+# Set up email server
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.comcast.net',
+  :domain => 'comcast.net'
+}
+CONTACT_RECIPIENT = "youremail@yourdomain.com"
